dd2729432bac08e2d90dde83baac06ac0406d74b
wannagetout <inv.moiseev@gmail.com>
2023-04-27 07:06:00 +0300

    Makes app frame

---
+++ entities/db_manager.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+class DBManager:
+
+    def __init__(self):
+        pass
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
