a55a9b0dbcd63445d5929428a9053bde4da42349
wannagetout <inv.moiseev@gmail.com>
2023-04-28 08:35:10 +0300

    Makes DB manager for add data into db, utils for get data and main file

---
+++ main.py
@@ -0,0 +1,6 @@
+from entities.db_manager import DBManager
+from utils import get_vacancies_json_from_request, get_employees_json_from_request
+
+
+if __name__ == '__main__':
+    pass
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from requests import request
 EMPLOYEES = [1740, 1057, 15478, 3529, 2381, 633069, 2180, 78638, 4496, 5778059]
 
 
-def get_json_from_request(employer_param: List[int]) -> List[request]:
+def get_vacancies_json_from_request(employer_param: List[int]) -> List[request]:
     url = "https://api.hh.ru/vacancies"
     params = {
         'area': range(1, 2),
@@ -14,4 +14,33 @@ def get_json_from_request(employer_param: List[int]) -> List[request]:
         'employer_id': employer_param
     }
     request_ = request(method='GET', url=url, params=params)
-    return [r for r in request_.json()['items']]
+    return [
+        [
+            {
+                'id': r['id'],
+                'name': r['name'],
+                'salary': r['salary']['from'] or r['salary']['to'],
+                'description': r['snippet']['requirement'],
+                'employer_id': r['employer']['id']
+            }
+        ] for r in request_.json()['items'] if r['salary']
+    ]
+
+
+def get_employees_json_from_request(employer_param: List) -> List[dict]:
+    employees_list: List = []
+    for employee_id in employer_param:
+        url = 'https://api.hh.ru/employers/{}'.format(employee_id)
+        request_ = request(method='GET', url=url).json()
+        employees_list.append(request_)
+    return [
+        {
+            'id': r['id'],
+            'name': r['name']
+        } for r in employees_list
+    ]
+
+
+reqes = get_employees_json_from_request(EMPLOYEES)
+for r in reqes:
+    print(r)
